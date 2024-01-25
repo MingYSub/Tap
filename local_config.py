c9a66395530becb4747831c68cdced1e65182280
MingYSub <mingysub@sina.com>
2024-01-25 16:47:29 +0800

    update config

--- local_config.py
+++ local_config.py
@@ -3,7 +3,6 @@
 config = {
     'merge': 'auto',  # 合并时间重复行，可填: 'none' 'auto' 'force'
     'clean_mode': True,  # 删除语气词
-    'fix_mode': True,  # 去除中括号
     'actor': False,  # 输出说话人
     'output_format': 'txt',  # 输出格式，可填: 'txt' 'ass' 'srt'
     'add_spaces': False  # 中西文之间添加六分之一空格（U+2006）
